af995f81a4285a84c3132f43756588715c8d6be2
breakfast1234 <truongmeric@gmail.com>
2018-08-29 14:29:43 -0700

    (WIP) Room Creation
    I'm trying to set the Node2D of Room.tscn to the center of the viewport, but it doesn't recognize Viewport.size
    (see debug when you run Room.tscn)

---
+++ Godot Stuff/Sprite.gd
@@ -0,0 +1,17 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	#position = Vector2(Viewport.size.x/2, Viewport.size.y/2)
+	#apply_scale(7)
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
